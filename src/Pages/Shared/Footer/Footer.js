d19aa6dd25f213094a0d1104d1e8f423ccdbe5ee
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-04-19 12:19:12 +0600

    dyinamic-footer-section

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,9 +4,44 @@ import './Footer.css';
 
 const Footer = () => {
    return (
-      <footer>
-         <p><small>Copyright © 2010-2025, All Rights Reserved.</small></p>
-      </footer>
+      <footer className=''>
+         <div className="footer-container">
+            <div className="mt-2 ms-5">
+               <p className="">Naama Wartenburg <br /> <small>Chief Marketing Officer</small><br />
+                  Contact: 9876543210</p>
+               <div className="mt-2">
+                  <p className="">Alauddin Imon <br /> <small>Developer & Head Programmer</small><br />
+                     Contact: 01845415506 <br />
+                     E-mail: alauddinemon3
+                     @gmail.com</p>
+               </div>
+            </div>
+            <div className="ms-5 mt-2">
+               <p className="">Michael Zur <br /> <small>Gloval Reach & Event Booking</small><br />
+                  Contact: 9876543210</p>
+               <p className="">Abu Hanif <br /> <small>Manager</small><br />
+                  Contact: 9876543210 <br />
+                  E-mail: abuhanif@gmail.com</p>
+            </div>
+         </div>
+
+
+         {/* Copyright-Contanier */}
+         <div className="copyright-container">
+
+            <div>
+               <p className='text-center'><small>Copyright © 2010-2025, All Rights Reserved.</small></p>
+            </div>
+
+            <div className="footer-img-contanier">
+               <img src='https://cdn-icons-png.flaticon.com/512/145/145802.png' alt="" />
+               <img src='https://cdn-icons-png.flaticon.com/512/145/145812.png' alt="" />
+               <img src='https://cdn-icons-png.flaticon.com/512/145/145808.png' alt="" />
+               <img className='rounded-pill' src='https://cdn-icons.flaticon.com/png/512/3669/premium/3669965.png?token=exp=1650348876~hmac=779ef9e053168adce1aa1dcab666a237' alt="" />
+            </div>
+
+         </div>
+      </footer >
    );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './Header.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
    return (
-      <header>
-         <h1>This is Header</h1>
-      </header>
+      <>
+         <Navbar className='navbar-contanier'>
+            <Container>
+               <Navbar.Brand href="#home">
+                  <img src='https://www.ynharari.com/wp-content/uploads/2020/01/ynhlogo.png' height="50" alt="" />
+               </Navbar.Brand>
+               <Nav className="ms-8">
+                  <Nav.Link to="/home">Home</Nav.Link>
+                  <Nav.Link to="/services">Services</Nav.Link>
+                  <Nav.Link to="/blogs">Blogs</Nav.Link>
+               </Nav>
+            </Container>
+         </Navbar>
+
+      </>
    );
 };
 
 export default Header;
+
+
