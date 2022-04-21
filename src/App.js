82722045ef61f20979107f62bd6d9cca183d1676
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-04-21 09:59:47 +0600

    Setup-React-Bootstrap-Responsive-Navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
-import Services from './Pages/Home/Services/Services';
 import Login from './Pages/Login/Login';
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/services" element={<Services />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
         <Route path="/blogs" element={<Blogs />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import './Header.css';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
    return (
-      <>
-         <div className="contanier">
-            <Navbar bg="primary" variant="dark">
-               <Container>
-                  <Navbar.Brand href="#home">
-                     <img src='https://www.ynharari.com/wp-content/uploads/2020/01/ynhlogo.png' height="50" alt="" />
-                  </Navbar.Brand>
-                  <Nav className="me-0">
-                     <Nav.Link href="#home">Home</Nav.Link>
-                     <Nav.Link href="#services">Services</Nav.Link>
-                     <Nav.Link href="#blogs">Blogs</Nav.Link>
-                  </Nav>
-               </Container>
-            </Navbar>
-         </div>
-
-
-      </>
-   );
+      <Navbar collapseOnSelect expand="lg" sticky="top" bg="primary" variant="dark">
+         <Container>
+            <Navbar.Brand as={Link} to="/">
+               <img src='https://www.ynharari.com/wp-content/uploads/2020/01/ynhlogo.png' height="50" alt="" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+               <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                  <Nav.Link as={Link} to="/checkin">Check-In</Nav.Link>
+                  <Nav.Link as={Link} to="/signup">SignUp</Nav.Link>
+                  <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                  <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                     <NavDropdown.Divider />
+                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                  </NavDropdown>
+               </Nav>
+            </Navbar.Collapse>
+         </Container>
+      </Navbar>);
 };
 
 export default Header;
